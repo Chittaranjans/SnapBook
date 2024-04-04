e333b3db2a017835b8667d8f7a875d5b156d3566
Chittaranjans <chittaedu18@gmail.com>
2024-04-04 22:45:56 +0530

    Signform_

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -8,9 +8,17 @@ const AuthLayout = () => {
       {isAuthenticated ? (
         <Navigate to="/" />
       ) : (
-        <section>
-          <Outlet />
-        </section>
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/b8baf867c630b66ea6f871ad579b9cc9.png"
+            alt="logo"
+            className=" x1:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
       )}
     </>
   );
